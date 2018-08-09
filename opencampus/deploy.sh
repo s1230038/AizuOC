8274553bf9652b531eddc7cec2838bf8862b5707
HT <HT@nowhere.com>
2018-08-09 09:53:44 +0000

    change deploy script

--- opencampus/deploy.sh
+++ opencampus/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+find ./ -name "*.php" | xargs -n1 php -l
+# /var/log/php_errors.log
 rm  -f /var/www/html/wp-admin/oc/*
 cp -r web/* /var/www/html/wp-admin/oc
 chown -R apache:apache /var/www/html/wp-admin/oc
-
-
+chmod 644 /var/www/html/wp-admin/oc/*
+ls -l /var/www/html/wp-admin/oc 
