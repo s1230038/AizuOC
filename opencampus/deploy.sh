545ec59fe84eccd50695bf8550f43a6bedb5e280
HT <HT@nowhere.com>
2018-08-10 02:45:41 +0000

    modify the deployment.

--- opencampus/deploy.sh
+++ opencampus/deploy.sh
@@ -6,6 +6,5 @@ mkdir /var/www/html/wp-admin/oc/
 cp -r web/* /var/www/html/wp-admin/oc
 chown -R apache:apache /var/www/html/wp-admin/oc
 chmod -R 775 /var/www/html/wp-admin/oc
-chmod 777 /var/www/html/wp-admin/oc/uploaded
 ls -ld /var/www/html/wp-admin/oc 
 ls -l /var/www/html/wp-admin/oc 
