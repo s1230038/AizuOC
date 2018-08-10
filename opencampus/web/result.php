aac50187f0ddbf4078ac255f2cba6d832148dbbe
HT <HT@nowhere.com>
2018-08-10 03:41:19 +0000

    output table

--- opencampus/web/result.php
+++ opencampus/web/result.php
@@ -1,3 +1,13 @@
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>手書き数字識別結果</title>
+  <meta name="description" content="The contents of the open campus in the Univ. of Aizu">
+  <meta name="author" content="An Aizu Student">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
 <?php
 session_start();
 $tempfile = $_FILES['fname']['tmp_name'];
@@ -7,12 +17,26 @@ $filename = './uploaded/' . $_SESSION["imgid"] . '-'
 if (is_uploaded_file($tempfile)) {
     if ( move_uploaded_file($tempfile , $filename )) {
         exec( 'python3.6 judgeNum.py ' . $filename , $out, $ret );
-        print_r( $out );
-        var_dump( $ret );
+        echo '<h1>あなたの描いた数字は：</h1>'; 
+        echo '<h2>' . $out[0]  . '</h2>';
+        echo '<h2> . $out[1]  . "</h2>';
+        echo '<table border="1">';
+        echo '<tr>';
+        echo '<th>数字：確率</th>';
+        echo '</tr>';
+        foreach( array_slice($out, 2) as $result ){
+          echo '<tr>';
+          echo '<td>' . $result . '</td>';
+          echo '</tr>';
+        }
+        echo '</table>';
     } else {
-        echo "ファイルをアップロードできません。";
+        echo 'ファイルをアップロードできません。';
     }
 } else {
-    echo "ファイルが選択されていません。";
+    echo 'ファイルが選択されていません。';
 } 
 ?>
+</body>
+
+</html>
