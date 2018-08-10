0148e80af2166ff59a1d59c520fb0cd23973e2e8
HT <HT@nowhere.com>
2018-08-10 01:42:22 +0000

    Modify the deploy script about the permission to consistent with other files.

--- opencampus/deploy.sh
+++ opencampus/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 find ./ -name "*.php" | xargs -n1 php -l
-# /var/log/php_errors.log
+# tail /var/log/php_errors.log
 rm  -rf /var/www/html/wp-admin/oc/*
 cp -r web/* /var/www/html/wp-admin/oc
 chown -R apache:apache /var/www/html/wp-admin/oc
-chmod 444 /var/www/html/wp-admin/oc/*
+chmod 775 /var/www/html/wp-admin/oc/*
 chmod 777 /var/www/html/wp-admin/oc/uploaded
 ls -l /var/www/html/wp-admin/oc 
