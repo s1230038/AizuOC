74106a978a554a5efb966e21d6e65eb7191cc75e
HT <HT@nowhere.com>
2018-08-10 04:04:21 +0000

    Change the location

--- opencampus/deploy.sh
+++ opencampus/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 find ./ -name "*.php" | xargs -n1 php -l
 # tail /var/log/php_errors.log
-rm  -rf /var/www/html/wp-admin/oc/
-mkdir /var/www/html/wp-admin/oc/
-cp -r web/* /var/www/html/wp-admin/oc
-chown -R apache:apache /var/www/html/wp-admin/oc
-chmod -R 775 /var/www/html/wp-admin/oc
-ls -ld /var/www/html/wp-admin/oc 
-ls -l /var/www/html/wp-admin/oc 
+rm  -rf /var/www/html/oc/
+mkdir /var/www/html/oc/
+cp -r web/* /var/www/html/oc
+chown -R apache:apache /var/www/html/oc
+chmod -R 775 /var/www/html/oc
+ls -ld /var/www/html/oc 
+ls -l /var/www/html/oc 
