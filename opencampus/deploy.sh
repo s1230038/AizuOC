3197fe640c100b3cb800b1fd93461035b531ab0c
HT <HT@nowhere.com>
2018-08-10 01:59:25 +0000

    modify deployment.

--- opencampus/deploy.sh
+++ opencampus/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 find ./ -name "*.php" | xargs -n1 php -l
 # tail /var/log/php_errors.log
-rm  -rf /var/www/html/wp-admin/oc/*
+rm  -rf /var/www/html/wp-admin/oc/
+mkdir /var/www/html/wp-admin/oc/
 cp -r web/* /var/www/html/wp-admin/oc
 chown -R apache:apache /var/www/html/wp-admin/oc
-chmod 775 /var/www/html/wp-admin/oc/*
+chmod -R 775 /var/www/html/wp-admin/oc
 chmod 777 /var/www/html/wp-admin/oc/uploaded
+ls -ld /var/www/html/wp-admin/oc 
 ls -l /var/www/html/wp-admin/oc 
