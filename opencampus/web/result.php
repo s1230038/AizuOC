f6067a9d6059407034d1217e190d6c2072eddc96
HT <HT@nowhere.com>
2018-08-16 08:50:39 +0000

    Modify comment about buffer overrun

--- opencampus/web/result.php
+++ opencampus/web/result.php
@@ -15,8 +15,8 @@ $filename = './uploaded/' . $_SESSION["imgid"] . '-' ;
 
 if (is_uploaded_file($tempfile)) {
     if ( move_uploaded_file($tempfile , $filename )) {
-        // Limit the file size with memory_limit and
-        // post_max_size in php.ini.
+        // Limit the file size with php.ini such as memory_limit,
+        // upload_max_filesize, etc to prevent the buffer overrun.
         exec( 'python3.6 judgeNum.py ' . $filename, $out, $ret );
         echo '<h1>あなたの描いた数字は：</h1>'; 
         echo '<h1>' . $out[0]  . '</h1>';
