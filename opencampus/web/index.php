7519bdbfd8e1bac02321dad13cf7f746e32c7909
HT <HT@nowhere.com>
2020-12-13 21:00:34 +0000

    Add Google Adsense tag and Google Analytics tag

--- opencampus/web/index.php
+++ opencampus/web/index.php
@@ -10,6 +10,22 @@
   <meta name="description" content="The contents of the open campus in the Univ. of Aizu">
   <meta name="author" content="An Aizu Student">
   <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109572615-2"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109572615-2');
+  </script>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8170054122978169",
+    enable_page_level_ads: true
+  });
+  </script>
 </head>
 
 <body>
